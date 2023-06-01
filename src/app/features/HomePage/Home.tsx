67f5e2acfa33ccec412dd560d642bf00a40767ce
ThanhLuong <thanhluong.it.0909@gmail.com>
2023-06-02 01:02:34 +0700

    test material UI

--- src/app/features/HomePage/Home.tsx
+++ src/app/features/HomePage/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Button } from '@mui/material';
 
-
 const Home = () => {
-  return <div>
-    <Button variant="contained" color="primary">
-      Hello Material-UI
-    </Button></div>;
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello Material-UI
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
